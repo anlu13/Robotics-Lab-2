dd96295cf4ab5857a3036e4d791e0f3f503bcb13
anlu13 <71241994+anlu13@users.noreply.github.com>
2024-02-26 01:29:30 -0500

    v1.5

--- Lab2/go_to_cube.py
+++ Lab2/go_to_cube.py
@@ -10,16 +10,18 @@ import os
 from glob import glob
 
 # needed for distance (in centimeters)
-from cozmo.util import degrees, distance_cm
+from cozmo.util import degrees, distance_mm
 
 from find_cube import *
 
-try:
-    from PIL import ImageDraw, ImageFont
-except ImportError:
-    sys.exit('run `pip3 install --user Pillow numpy` to run this example')
-def nothing(x):
-    pass
+from PIL import ImageDraw, ImageFont
+
+#try:
+    #from PIL import ImageDraw, ImageFont
+#except ImportError:
+    #sys.exit('run `pip3 install --user Pillow numpy` to run this example')
+#def nothing(x):
+    #pass
 
 YELLOW_LOWER = np.array([9, 115, 151])
 YELLOW_UPPER = np.array([179, 215, 255])
@@ -87,21 +89,21 @@ async def run(robot: cozmo.robot.Robot):
                 # if cube
                 if cube:
                     # calculate cm distance from Cozmo to cube
-                    distance_to_cube_cm = cube[2]
-                    print("distance_to_cube_cm: ", distance_to_cube_cm)
+                    distance_to_cube_mm = cube[2]
+                    print("distance_to_cube_mm: ", distance_to_cube_mm)
 
                     # if next to cube (within 5cm/2inch)
-                    if distance_to_cube_cm < 4.0:
+                    if distance_to_cube_mm < 40.0:
                         robot.say_text("Found Cube. Within 5 centimeters.").wait_for_completed()
                     else: # if far from cube
                         # go to cube ("drive to within 5cm (2 inches) of it")
-                        action = robot.go_to_object(cube, distance_cm(4.0))
+                        action = robot.go_to_object(cube, distance_mm(40.0))
                         action.wait_for_completed()
 
                 # if no cube
                 else: 
                     # spin to find cube
-                    robot.turn_in_place(degrees(360)).wait_for_completed()
+                    robot.turn_in_place(degrees(15)).wait_for_completed()
 
 
 
